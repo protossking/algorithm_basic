ef74df621e7ddb5d749c2de984cf39f05a0be31f
upgrademarine5 <upgrademarine5@icloud.com>
2021-10-03 16:19:19 +0900

    10 03 섬의개수 양

--- src/Main2.java
+++ src/Main2.java
@@ -1,149 +1,229 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
-public class Main2 {
-
-    static int n;
-    static int m;
-    static int[][] map;
-    static int d;
-    static int[][] delta = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}}; //북 서 남 동
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(in.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        map = new int[n][m];
-
-        st = new StringTokenizer(in.readLine());
-
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(in.readLine());
-            for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int k = d;
-//        k = k + 1;
-        while (true) {
-            // 청소
-            map[r][c] = 2;
-
-            // 방향입력받아 돌리기
-
-            int nr = r + delta[k % 3][0];
-            int nc = c + delta[k % 3][1];
-
-            if (isIn(nr, nc)) {
-
-                if (map[nr][nc] == 0) {
-                    r = nr;
-                    c = nc;
-                    map[nr][nc] = 2;
-                    continue;
-
-
-                }
-                else if(map[nr][nc] == 1 || map[nr][nc] == 2) {
-                    boolean flag = false;
-
-                    for(int i =0 ; i < 4; i++) {
-                        int a = r + delta[i][0];
-                        int b = c + delta[i][1];
-
-                        if(map[a][b] != 0) {
-                            flag = true;
-                        } else {
-                            flag = false;
-                            break;
-                        }
-                    }
-
-                    if(flag) {
-                        int a = r - delta[k%3][0];
-                        int b = c - delta[k%3][1];
-                        if(map[a][b] == 1) {
-                            break;
-                        } else {
-                            r = a;
-                            c = b;
-                            continue;
-                        }
-                    } else {
-                        k ++;
-                        continue;
-                    }
-                }
-
-            }
-
-
-
-
-
-
-        }
-
-        int cnt = 0;
-        for (
-                int i = 0;
-                i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == 2) {
-                    cnt++;
-                }
-            }
-        }
-        System.out.println(cnt);
-    }
-
-    static boolean isIn(int r, int c) {
-        if (r < 0 || c < 0 || r >= n || c >= m) {
-            return false;
-        }
-        return true;
-    }
-
-    static boolean all(int r, int c) {
-        if (map[r - 1][c] == 0 || map[r][c - 1] == 0 || map[r + 1][c] == 0 || map[r][c + 1] == 0) {
-            return false;
-        }
-
-        return true;
-    }
-
-
-    static int rotate(int dir) {
-
-        // 북쪽이면 서쪽으로
-        if (dir == 0) {
-            return 2;
-        }
-
-        // 서쪽이면 남쪽으로
-        else if (dir == 1) {
-            return 1;
-        }
-
-        // 남쪽이면 동쪽으로
-        else if (dir == 2) {
-            return 3;
-        }
-        // 동쪽이면 북쪽으로
-        else {
-            return 0;
-        }
-    }
-
-}
+//t java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.LinkedList;
+//import java.util.Queue;
+//import java.util.StringTokenizer;
+//
+//public class Solution_탈주범검거 {
+//    static int[] di = { 0, 1, 0, -1 };
+//    static int[] dj = { 1, 0, -1, 0 };
+//    static StringTokenizer st;
+//    static int[][] map;
+//    static boolean[][] visited;
+//    static int TC, N, M, start_i, start_j, next_i, next_j, time, ans;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        TC = Integer.parseInt(br.readLine().trim());
+//        for (int tc = 1; tc <= TC; tc++) {
+//            st = new StringTokenizer(br.readLine().trim(), " ");
+//            N = Integer.parseInt(st.nextToken());
+//            M = Integer.parseInt(st.nextToken());
+//            start_i = Integer.parseInt(st.nextToken());
+//            start_j = Integer.parseInt(st.nextToken());
+//            time = Integer.parseInt(st.nextToken());
+//
+//            map = new int[N][M];
+//            for (int i = 0; i < N; i++) {
+//                st = new StringTokenizer(br.readLine(), " ");
+//                for (int j = 0; j < M; j++) {
+//                    map[i][j] = Integer.parseInt(st.nextToken());
+//                }
+//            }
+//
+//            ans = 1;
+//            bfs(new Pos(start_i, start_j, time - 1));
+//            sb.append("#" + tc + " " + ans + "\n");
+//        }
+//        System.out.println(sb.toString());
+//    }
+//
+//    static void bfs(Pos before) {
+//        Queue<Pos> q = new LinkedList<Pos>();
+//        q.offer(before);
+//        visited = new boolean[N][M];
+//        visited[before.i][before.j] = true;
+//
+//        while (!q.isEmpty()) {
+//            Pos tmp = q.poll();
+//            int cap = map[tmp.i][tmp.j];
+//
+//            if (cap == 1) {
+//                for (int k = 0; k < 4; k++) {
+//                    next_i = tmp.i + di[k];
+//                    next_j = tmp.j + dj[k];
+//
+//                    if (next_i < 0 || next_j < 0 || next_i >= N || next_j >= M)
+//                        continue;
+//
+//                    if(k == 0 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 4 || map[next_i][next_j] == 5))
+//                        continue;
+//                    else if(k == 1 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 5 || map[next_i][next_j] == 6))
+//                        continue;
+//                    else if( k == 2 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 6 || map[next_i][next_j] == 7))
+//                        continue;
+//                    else if( k == 3 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 4 || map[next_i][next_j] == 7))
+//                        continue;
+//
+//                    if (!visited[next_i][next_j] && tmp.dist > 0 && map[next_i][next_j] != 0) {
+//                        visited[next_i][next_j] = true;
+//                        q.offer(new Pos(next_i, next_j, tmp.dist - 1));
+//                        ans++;
+//                    }
+//                }
+//            } else if (cap == 2) {
+//                for (int k = 1; k < 4; k += 2) {
+//                    next_i = tmp.i + di[k];
+//                    next_j = tmp.j + dj[k];
+//
+//                    if (next_i < 0 || next_j < 0 || next_i >= N || next_j >= M)
+//                        continue;
+//
+//                    if(k == 0 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 4 || map[next_i][next_j] == 5))
+//                        continue;
+//                    else if(k == 1 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 5 || map[next_i][next_j] == 6))
+//                        continue;
+//                    else if( k == 2 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 6 || map[next_i][next_j] == 7))
+//                        continue;
+//                    else if( k == 3 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 4 || map[next_i][next_j] == 7))
+//                        continue;
+//
+//                    if (!visited[next_i][next_j] && tmp.dist > 0 && map[next_i][next_j] != 0) {
+//                        visited[next_i][next_j] = true;
+//                        q.offer(new Pos(next_i, next_j, tmp.dist - 1));
+//                        ans++;
+//                    }
+//                }
+//            } else if (cap == 3) {
+//                for (int k = 0; k < 4; k += 2) {
+//                    next_i = tmp.i + di[k];
+//                    next_j = tmp.j + dj[k];
+//
+//                    if (next_i < 0 || next_j < 0 || next_i >= N || next_j >= M)
+//                        continue;
+//
+//                    if(k == 0 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 4 || map[next_i][next_j] == 5))
+//                        continue;
+//                    else if(k == 1 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 5 || map[next_i][next_j] == 6))
+//                        continue;
+//                    else if( k == 2 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 6 || map[next_i][next_j] == 7))
+//                        continue;
+//                    else if( k == 3 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 4 || map[next_i][next_j] == 7))
+//                        continue;
+//
+//                    if (!visited[next_i][next_j] && tmp.dist > 0 && map[next_i][next_j] != 0) {
+//                        visited[next_i][next_j] = true;
+//                        q.offer(new Pos(next_i, next_j, tmp.dist - 1));
+//                        ans++;
+//                    }
+//                }
+//            } else if (cap == 4) {
+//                for (int k = 0; k < 4; k += 3) {
+//                    next_i = tmp.i + di[k];
+//                    next_j = tmp.j + dj[k];
+//
+//                    if (next_i < 0 || next_j < 0 || next_i >= N || next_j >= M)
+//                        continue;
+//
+//                    if(k == 0 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 4 || map[next_i][next_j] == 5))
+//                        continue;
+//                    else if(k == 1 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 5 || map[next_i][next_j] == 6))
+//                        continue;
+//                    else if( k == 2 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 6 || map[next_i][next_j] == 7))
+//                        continue;
+//                    else if( k == 3 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 4 || map[next_i][next_j] == 7))
+//                        continue;
+//
+//                    if (!visited[next_i][next_j] && tmp.dist > 0 && map[next_i][next_j] != 0) {
+//                        visited[next_i][next_j] = true;
+//                        q.offer(new Pos(next_i, next_j, tmp.dist - 1));
+//                        ans++;
+//                    }
+//                }
+//            } else if (cap == 5) {
+//                for (int k = 0; k < 2; k++) {
+//                    next_i = tmp.i + di[k];
+//                    next_j = tmp.j + dj[k];
+//
+//                    if (next_i < 0 || next_j < 0 || next_i >= N || next_j >= M)
+//                        continue;
+//
+//                    if(k == 0 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 4 || map[next_i][next_j] == 5))
+//                        continue;
+//                    else if(k == 1 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 5 || map[next_i][next_j] == 6))
+//                        continue;
+//                    else if( k == 2 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 6 || map[next_i][next_j] == 7))
+//                        continue;
+//                    else if( k == 3 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 4 || map[next_i][next_j] == 7))
+//                        continue;
+//
+//                    if (!visited[next_i][next_j] && tmp.dist > 0 && map[next_i][next_j] != 0) {
+//                        visited[next_i][next_j] = true;
+//                        q.offer(new Pos(next_i, next_j, tmp.dist - 1));
+//                        ans++;
+//                    }
+//                }
+//            } else if (cap == 6) {
+//                for (int k = 1; k < 3; k++) {
+//                    next_i = tmp.i + di[k];
+//                    next_j = tmp.j + dj[k];
+//
+//                    if (next_i < 0 || next_j < 0 || next_i >= N || next_j >= M)
+//                        continue;
+//
+//                    if(k == 0 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 4 || map[next_i][next_j] == 5))
+//                        continue;
+//                    else if(k == 1 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 5 || map[next_i][next_j] == 6))
+//                        continue;
+//                    else if( k == 2 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 6 || map[next_i][next_j] == 7))
+//                        continue;
+//                    else if( k == 3 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 4 || map[next_i][next_j] == 7))
+//                        continue;
+//
+//                    if (!visited[next_i][next_j] && tmp.dist > 0 && map[next_i][next_j] != 0) {
+//                        visited[next_i][next_j] = true;
+//                        q.offer(new Pos(next_i, next_j, tmp.dist - 1));
+//                        ans++;
+//                    }
+//                }
+//            } else if (cap == 7) {
+//                for (int k = 2; k < 4; k++) {
+//                    next_i = tmp.i + di[k];
+//                    next_j = tmp.j + dj[k];
+//
+//                    if (next_i < 0 || next_j < 0 || next_i >= N || next_j >= M)
+//                        continue;
+//
+//                    if(k == 0 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 4 || map[next_i][next_j] == 5))
+//                        continue;
+//                    else if(k == 1 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 5 || map[next_i][next_j] == 6))
+//                        continue;
+//                    else if( k == 2 && (map[next_i][next_j] == 2 || map[next_i][next_j] == 6 || map[next_i][next_j] == 7))
+//                        continue;
+//                    else if( k == 3 && (map[next_i][next_j] == 3 || map[next_i][next_j] == 4 || map[next_i][next_j] == 7))
+//                        continue;
+//
+//                    if (!visited[next_i][next_j] && tmp.dist > 0 && map[next_i][next_j] != 0) {
+//                        visited[next_i][next_j] = true;
+//                        q.offer(new Pos(next_i, next_j, tmp.dist - 1));
+//                        ans++;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    static class Pos {
+//        int i, j, dist;
+//
+//        public Pos(int i, int j, int dist) {
+//            super();
+//            this.i = i;
+//            this.j = j;
+//            this.dist = dist;
+//        }
+//    }
+//}
--- src/알고리즘이론/bfsdfs/SWEA_모의_탈주범검거.java
+++ src/알고리즘이론/bfsdfs/SWEA_모의_탈주범검거.java
@@ -1,8 +1,264 @@
 package 알고리즘이론.bfsdfs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class SWEA_모의_탈주범검거 {
 
-    public static void main(String[] args) {
+    static int N;
+    static int M;
+    static int R;
+    static int C;
+    static int L;
 
+    static int[][] map;
+    static boolean[][] visited;
+
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int ans;
+
+    static class Point {
+        int r;
+        int c;
+        int dist;
+
+        public Point(int r, int c, int dist) {
+            this.r = r;
+            this.c = c;
+            this.dist = dist;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(in.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int tc=1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            R = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+
+            map = new int[N][M];
+            visited = new boolean[N][M];
+
+            int cnt = 0;
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(in.readLine());
+                for (int j = 0; j < M; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            bfs(R, C, L);
+//            System.out.println(ans);
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    if(visited[i][j]){
+                        cnt++;
+                    }
+                }
+            }
+
+            sb.append("#").append(tc).append(" ").append(cnt).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+    static void bfs(int k, int l, int time) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(k, l, time-1));
+        visited[k][l] = true;
+
+
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+            int r = p.r;
+            int c = p.c;
+
+            int pipe = map[r][c];
+
+
+
+            if(pipe == 1) {
+                for(int adj = 0; adj < 4; adj++) {
+                    int nr = r + dr[adj];
+                    int nc = c + dc[adj];
+
+                    if(nr < 0 || nc < 0 || nr >= N || nc >= M) {
+                        continue;
+                    }
+                    if((adj == 0) && (map[nr][nc] == 3 || map[nr][nc] == 4 || map[nr][nc] == 7)) continue;
+                    else if((adj == 1) && (map[nr][nc] == 3 || map[nr][nc] == 5 || map[nr][nc] == 6)) continue;
+                    else if((adj == 2) && (map[nr][nc] == 2 || map[nr][nc] == 6 || map[nr][nc] == 7)) continue;
+                    else if((adj == 3) && (map[nr][nc] == 2 || map[nr][nc] == 4 || map[nr][nc] == 5)) continue;
+
+
+                    if(!visited[nr][nc] && p.dist > 0 && map[nr][nc] != 0) {
+                        visited[nr][nc] = true;
+                        queue.offer(new Point(nr,nc,p.dist -1));
+
+
+                    }
+
+
+                }
+
+            }
+            else if(pipe == 2) {
+                for(int adj = 0; adj < 2; adj++) {
+                    int nr = r + dr[adj];
+                    int nc = c + dc[adj];
+
+                    if(nr < 0 || nc < 0 || nr >= N || nc >= M) {
+                        continue;
+                    }
+                    if((adj == 0) && (map[nr][nc] == 3 || map[nr][nc] == 4 || map[nr][nc] == 7)) continue;
+                    else if((adj == 1) && (map[nr][nc] == 3 || map[nr][nc] == 5 || map[nr][nc] == 6)) continue;
+                    else if((adj == 2) && (map[nr][nc] == 2 || map[nr][nc] == 6 || map[nr][nc] == 7)) continue;
+                    else if((adj == 3) && (map[nr][nc] == 2 || map[nr][nc] == 4 || map[nr][nc] == 5)) continue;
+
+
+                    if(!visited[nr][nc] && p.dist > 0 && map[nr][nc] != 0) {
+                        visited[nr][nc] = true;
+                        queue.offer(new Point(nr,nc,p.dist -1));
+
+
+                    }
+
+                }
+
+            }
+            else if(pipe == 3) {
+                for(int adj = 2; adj < 4; adj ++) {
+                    int nr = r + dr[adj];
+                    int nc = c + dc[adj];
+
+                    if(nr < 0 || nc < 0 || nr >= N || nc >= M) {
+                        continue;
+                    }
+                    if((adj == 0) && (map[nr][nc] == 3 || map[nr][nc] == 4 || map[nr][nc] == 7)) continue;
+                    else if((adj == 1) && (map[nr][nc] == 3 || map[nr][nc] == 5 || map[nr][nc] == 6)) continue;
+                    else if((adj == 2) && (map[nr][nc] == 2 || map[nr][nc] == 6 || map[nr][nc] == 7)) continue;
+                    else if((adj == 3) && (map[nr][nc] == 2 || map[nr][nc] == 4 || map[nr][nc] == 5)) continue;
+
+
+                    if(!visited[nr][nc] && p.dist > 0 && map[nr][nc] != 0) {
+                        visited[nr][nc] = true;
+                        queue.offer(new Point(nr,nc,p.dist -1));
+
+
+                    }
+                }
+
+            }
+            else if(pipe == 4) {
+
+                for(int adj = 0; adj < 4; adj +=3) {
+                    int nr = r + dr[adj];
+                    int nc = c + dc[adj];
+
+                    if(nr < 0 || nc < 0 || nr >= N || nc >= M) {
+                        continue;
+                    }
+                    if((adj == 0) && (map[nr][nc] == 3 || map[nr][nc] == 4 || map[nr][nc] == 7)) continue;
+                    else if((adj == 1) && (map[nr][nc] == 3 || map[nr][nc] == 5 || map[nr][nc] == 6)) continue;
+                    else if((adj == 2) && (map[nr][nc] == 2 || map[nr][nc] == 6 || map[nr][nc] == 7)) continue;
+                    else if((adj == 3) && (map[nr][nc] == 2 || map[nr][nc] == 4 || map[nr][nc] == 5)) continue;
+
+
+                    if(!visited[nr][nc] && p.dist > 0 && map[nr][nc] != 0) {
+                        visited[nr][nc] = true;
+                        queue.offer(new Point(nr,nc,p.dist -1));
+
+
+                    }
+                }
+
+
+            }
+            else if(pipe == 5) {
+
+                for(int adj = 1; adj < 4; adj +=2) {
+                    int nr = r + dr[adj];
+                    int nc = c + dc[adj];
+
+                    if(nr < 0 || nc < 0 || nr >= N || nc >= M) {
+                        continue;
+                    }
+                    if((adj == 0) && (map[nr][nc] == 3 || map[nr][nc] == 4 || map[nr][nc] == 7)) continue;
+                    else if((adj == 1) && (map[nr][nc] == 3 || map[nr][nc] == 5 || map[nr][nc] == 6)) continue;
+                    else if((adj == 2) && (map[nr][nc] == 2 || map[nr][nc] == 6 || map[nr][nc] == 7)) continue;
+                    else if((adj == 3) && (map[nr][nc] == 2 || map[nr][nc] == 4 || map[nr][nc] == 5)) continue;
+
+
+                    if(!visited[nr][nc] && p.dist > 0 && map[nr][nc] != 0) {
+                        visited[nr][nc] = true;
+                        queue.offer(new Point(nr,nc,p.dist -1));
+
+
+                    }
+                }
+
+
+            }
+            else if(pipe == 6) {
+
+                for(int adj = 1; adj < 3; adj ++) {
+                    int nr = r + dr[adj];
+                    int nc = c + dc[adj];
+
+                    if(nr < 0 || nc < 0 || nr >= N || nc >= M) {
+                        continue;
+                    }
+                    if((adj == 0) && (map[nr][nc] == 3 || map[nr][nc] == 4 || map[nr][nc] == 7)) continue;
+                    else if((adj == 1) && (map[nr][nc] == 3 || map[nr][nc] == 5 || map[nr][nc] == 6)) continue;
+                    else if((adj == 2) && (map[nr][nc] == 2 || map[nr][nc] == 6 || map[nr][nc] == 7)) continue;
+                    else if((adj == 3) && (map[nr][nc] == 2 || map[nr][nc] == 4 || map[nr][nc] == 5)) continue;
+
+
+                    if(!visited[nr][nc] && p.dist > 0 && map[nr][nc] != 0) {
+                        visited[nr][nc] = true;
+                        queue.offer(new Point(nr,nc,p.dist -1));
+
+
+                    }
+                }
+
+            }
+            else if(pipe == 7) {
+
+                for(int adj = 0; adj < 3; adj +=2) {
+                    int nr = r + dr[adj];
+                    int nc = c + dc[adj];
+
+                    if(nr < 0 || nc < 0 || nr >= N || nc >= M) {
+                        continue;
+                    }
+                    if((adj == 0) && (map[nr][nc] == 3 || map[nr][nc] == 4 || map[nr][nc] == 7)) continue;
+                    else if((adj == 1) && (map[nr][nc] == 3 || map[nr][nc] == 5 || map[nr][nc] == 6)) continue;
+                    else if((adj == 2) && (map[nr][nc] == 2 || map[nr][nc] == 6 || map[nr][nc] == 7)) continue;
+                    else if((adj == 3) && (map[nr][nc] == 2 || map[nr][nc] == 4 || map[nr][nc] == 5)) continue;
+
+
+                    if(!visited[nr][nc] && p.dist > 0 && map[nr][nc] != 0) {
+                        visited[nr][nc] = true;
+                        queue.offer(new Point(nr,nc,p.dist -1));
+
+
+                    }
+                }
+
+            }
+
+        }
     }
 }
